c55470b81d61e2d0ae61f91d6c8dfeeb45cb3ea9
Stavros Kladis <stavroskladis@hotmail.com>
2025-10-21 07:11:12 +0200

    combine binary and linear search plus fix error in printing login

--- topics/02-Searching/searching.cpp
+++ topics/02-Searching/searching.cpp
@@ -7,6 +7,25 @@
 #include "../../includes/utils.hpp"
 
 /**
+ * Linear searching on a vector.
+ *  - Best Case Time Complexity O(1)
+ *  - Worst Case Time Complexity O(n)
+ *  - Average Time Complexity O(n)
+ *  - Space Complexity O(1)
+ */
+template <typename T>
+int linear_search(const std::vector<T>& A, T target) {
+
+    for (size_t i = 0; i < A.size(); ++i) {
+        if (A[i] == target) {
+            return static_cast<int>(i);
+        }
+    }
+
+    return -1;
+}
+
+/**
  * Binary searching on a vector.
  *  - Best Case Time Complexity O(1)
  *  - Worst Case Time Complexity O(log n)
@@ -14,7 +33,7 @@
  *  - Space Complexity O(1)
  */
 template <typename T>
-int binary_search(std::vector<T>& A, T target) {
+int binary_search(const std::vector<T>& A, T target) {
     int low = 0;
     int high = A.size() - 1;
 
@@ -47,16 +66,24 @@ int main() {
     print_vector<int>(nums);
 
     int target = 5;
+    int idx = linear_search(nums, target);
+
+    std::cout << "Linear searching" << std::endl;
+    std::cout << "\nTarget " << target
+              << (idx >= 0 ? " found at index " + std::to_string(idx)
+                           : " not found")
+              << std::endl;
 
     std::sort(nums.begin(), nums.end());
     std::cout << "\nSorted array" << std::endl;
     print_vector<int>(nums);
 
-    int idx = binary_search(nums, target);
+    idx = binary_search(nums, target);
 
+    std::cout << "Binary searching" << std::endl;
     std::cout << "\nTarget " << target
-              << (idx > 0 ? " found at index " + std::to_string(idx)
-                          : " not found")
+              << (idx >= 0 ? " found at index " + std::to_string(idx)
+                           : " not found")
               << std::endl;
 
     return 0;
