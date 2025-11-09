c3c7f5993b56c84889abc75870392bf1dcc6e095
Stavros Kladis <stavroskladis@hotmail.com>
2025-11-10 00:07:30 +0100

    add a basic linked list template

---
+++ topics/03-Linked_Lists/linked_lists.hpp
@@ -0,0 +1,72 @@
+#ifndef LINKED_LISTS_H
+#define LINKED_LISTS_H
+
+#include <concepts>
+#include <iostream>
+#include <memory>
+
+// Concept to ensure T is printable (has operator<< for std::ostream)
+template <typename T>
+concept Printable = requires(std::ostream& os, const T& t) { os << t; };
+
+template <Printable T>
+class Node {
+  private:
+    T data;
+
+  public:
+    std::unique_ptr<Node<T>> next;
+
+    Node(const T& value) : data(value), next(nullptr) {}
+
+    // Getter for data
+    const T& getData() const {
+        return data;
+    }
+    T& getData() {
+        return data;
+    }
+
+    // Stream output operator (friend function)
+    friend std::ostream& operator<<(std::ostream& os, const Node<T>& node) {
+        os << node.data;
+        return os;
+    }
+};
+
+template <Printable T>
+class LinkedList {
+  private:
+    std::unique_ptr<Node<T>> head;
+
+  public:
+    LinkedList() : head(nullptr) {}
+
+    // Allowing recursive destruction of the Nodes can overflow the stack.
+    // To pevent this we need an iterative destructor
+    ~LinkedList() {
+        while (head) {
+            auto next = std::move(head->next);
+            head = std::move(next);
+        }
+    }
+
+    // Add a new node to the front of the list
+    void add(const T& data) {
+        auto new_node = std::make_unique<Node<T>>(data);
+        new_node->next = std::move(head);
+        head = std::move(new_node);
+    }
+
+    // Traverse the list and print all elements
+    void print() const {
+        const Node<T>* current = head.get();
+        while (current) {
+            std::cout << *current << " "; // Use operator<<
+            current = current->next.get();
+        }
+        std::cout << std::endl;
+    }
+};
+
+#endif
