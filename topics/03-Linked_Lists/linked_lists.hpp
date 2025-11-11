de6ac86731357f0c0c53111fb98524c5ba316948
Stavros Kladis <stavroskladis@hotmail.com>
2025-11-11 23:20:42 +0100

    c++ best practices

--- topics/03-Linked_Lists/linked_lists.hpp
+++ topics/03-Linked_Lists/linked_lists.hpp
@@ -1,5 +1,5 @@
-#ifndef LINKED_LISTS_H
-#define LINKED_LISTS_H
+#ifndef LINKED_LISTS_HPP
+#define LINKED_LISTS_HPP
 
 #include <concepts>
 #include <iostream>
@@ -9,7 +9,7 @@
 template <typename T>
 concept Printable = requires(std::ostream& os, const T& t) { os << t; };
 
-template <Printable T>
+template <typename T>
 class Node {
   private:
     T data;
@@ -17,42 +17,85 @@ class Node {
   public:
     std::unique_ptr<Node<T>> next;
 
-    Node(const T& value) : data(value), next(nullptr) {}
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+    explicit Node(T&& value) : data(std::move(value)), next(nullptr) {}
+    Node(Node&& other) noexcept
+        : data(std::move(other.data)), next(std::move(other.next)) {}
+    Node& operator=(Node&& other) noexcept {
+        if (this != &other) {
+            data = std::move(other.data);
+            next = std::move(other.next);
+        }
+        return *this;
+    }
+    // Copy Node ctor and copy Node assignment are auto deleted due to unique_ptr
 
-    // Getter for data
-    const T& getData() const {
+    constexpr const T& getData() const noexcept {
         return data;
     }
-    T& getData() {
+    constexpr T& getData() noexcept {
         return data;
     }
 
-    // Stream output operator (friend function)
-    friend std::ostream& operator<<(std::ostream& os, const Node<T>& node) {
+    // Stream output operator for Node.
+    // Requires T to be printable (built-in types work automatically,
+    // custom types need operator<< overloaded).
+    friend std::ostream& operator<<(std::ostream& os, const Node<T>& node)
+        requires Printable<T>
+    {
         os << node.data;
         return os;
     }
+
+    friend bool operator==(const Node<T>& lhs, const Node<T>& rhs) {
+        return lhs.data == rhs.data;
+    }
+
+    // DRY principle, consistency  and maintainability, use operator==
+    // If we change the equality operator, no change needed here
+    friend bool operator!=(const Node<T>& lhs, const Node<T>& rhs) {
+        return !(lhs == rhs);
+    }
 };
 
-template <Printable T>
+template <typename T>
 class LinkedList {
   private:
     std::unique_ptr<Node<T>> head;
 
-  public:
-    LinkedList() : head(nullptr) {}
-
-    // Allowing recursive destruction of the Nodes can overflow the stack.
-    // To pevent this we need an iterative destructor
-    ~LinkedList() {
+    void clear_impl() {
         while (head) {
             auto next = std::move(head->next);
             head = std::move(next);
         }
     }
 
-    // Traverse the list and print all elements
-    void print() const {
+  public:
+    constexpr LinkedList() : head(nullptr) {}
+
+    // Move constructor and move assignment are explicitly deleted
+    LinkedList(LinkedList&&) = delete;
+    LinkedList& operator=(LinkedList&&) = delete;
+
+    // Copy constructor and copy assignment are auto deleted due to unique_ptr
+
+    // Allowing recursive destruction of the Nodes can overflow the stack.
+    // To prevent this we need an iterative destructor
+    ~LinkedList() {
+        clear_impl();
+    }
+
+    void clear() noexcept {
+        clear_impl();
+    }
+
+    // Traverse the list and print all elements.
+    // We constrain only the print method instead of the class for flexibility.
+    // Built-in types (int, string, etc.) work automatically.
+    // Custom types require an overloaded operator<< for std::ostream.
+    void print() const
+        requires Printable<T>
+    {
         const Node<T>* current = head.get();
 
         while (current) {
@@ -62,19 +105,19 @@ class LinkedList {
         std::cout << std::endl;
     }
 
-    int count() const {
+    constexpr int size() const noexcept {
         const Node<T>* current = head.get();
-        int count = 0;
+        int size = 0;
 
         while (current) {
-            ++count;
+            ++size;
             current = current->next.get();
         }
 
-        return count;
+        return size;
     }
 
-    bool empty() const {
+    constexpr bool empty() const noexcept {
         return head == nullptr;
     }
 
